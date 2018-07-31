a6bb4b200fba5e849438019e44ecb6951209d712
Johan Denoyer <johan.denoyer@saagie.com>
2018-07-31 15:17:22 +0200

    Cleanup code

--- french_dmr_repeater.py
+++ french_dmr_repeater.py
@@ -3,9 +3,7 @@
 
 import requests
 import json
-r = requests.get("https://api.brandmeister.network/v1.0/repeater/?action=LIST")
-data = r.json()
-for key in r.json():
+for key in (requests.get("https://api.brandmeister.network/v1.0/repeater/?action=LIST")).json():
     if (key['repeaterid'].find("208")== 0) : # If 208 found at start of repeaterid, then it is French and we go on.
         if (int(key['status'])<4): # Filter out DMR Hotspot
             print(json.dumps(key)) # Print JSON format
